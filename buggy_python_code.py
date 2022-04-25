6a5d0371bb7561fe65d627220b5ccfe53f7103ec
Michal Sokolik <xsokolik@fi.muni.cz>
2022-04-25 14:51:56 +0200

    feat: added docstrings

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -9,6 +9,7 @@ import flask
 
 
 def transcode_file(_, filename):
+    """Transcode the file"""
     command = 'ffmpeg -i "{source}" output_file.mpg'\
         .format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
@@ -16,22 +17,28 @@ def transcode_file(_, filename):
 
 # Assert statements
 def assert_statements(_, user):
+    """Assert statements"""
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
 class RunBinSh(object):
+    """Class for BinSh"""
+
     def __reduce__(self):
+        """Reduce"""
         return (subprocess.Popen, (('/bin/sh',),))
 
 
 def import_urlib_version(version):
+    """Import urlib"""
     exec("import urllib%s as urllib" % version)
 
 
 @app.route('/')
 def index():
+    """Run flask"""
     module = flask.request.args.get("module")
     import_urlib_version(module)
 
